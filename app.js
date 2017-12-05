e1c39a9a3f4d5e013599b6eeff5cb783514be372
kamal <kamal-allali@hotmail.fr>
2017-12-05 22:24:18 +0100

    socketio

--- app.js
+++ app.js
@@ -20,20 +20,6 @@ cookie = require('cookie');
 // configure dotenv
 require('dotenv').load();
 
-io.configure('production', function(){
-    console.log(doodlr + " set config for production");
-    io.enable('browser client minification');  // send minified client
-    io.enable('browser client etag');          // apply etag caching logic based on version number
-    io.enable('browser client gzip');          // gzip the file
-    io.set('log level', 1);                    // reduce logging
-    io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-        'websocket'
-      , 'flashsocket'
-      , 'htmlfile'
-      , 'xhr-polling'
-      , 'jsonp-polling'
-    ]);
-  });
 
 var cookies = cookie.parse('couleur=bleu');
 
